048d34cc8495e9344518099d57aab1b7f5c14c36
Daksh Batra <dakshbatraofficial@gmail.com>
2026-02-10 05:08:24 +0530

    Speed Control and Crawl Mode Addition

--- code/live_pred.py
+++ code/live_pred.py
@@ -2,6 +2,7 @@ import joblib
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
 
 model = joblib.load("hand_gesture_model.pkl")
 le = joblib.load("label_encoder.pkl")
@@ -16,7 +17,8 @@ hands=mp_hands.Hands(static_image_mode=False,
 
 cam = cv2.VideoCapture(0)
 
-threshold = 0.7
+normal_threshold = 0.70
+crawl_theshold = 0.59
 
 while True:
     success, image=cam.read()
@@ -27,6 +29,10 @@ while True:
 
     predicted="STOP(NO ACTION)"
     confidence=0.0
+    speed_lvl="NONE"
+    speed=0.0
+    control_mode="NO HAND"
+    width=0.0
 
     if result.multi_hand_landmarks: #if hand detecting
         for hand_landmarks in result.multi_hand_landmarks:
@@ -35,8 +41,14 @@ while True:
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS
             )
+            lms = hand_landmarks.landmark  #index finger joint 5 and pinky finger joint is 17
+            x5,y5=lms[5].x ,lms[5].y
+            x17,y17=lms[17].x ,lms[17].y
+            width=math.sqrt((x5-x17)**2 + (y5-y17)**2) 
+
+            far=width<0.16
             tracer_list=[]
-            for lm in hand_landmarks.landmark:
+            for lm in lms:
                 tracer_list.append(lm.x)
                 tracer_list.append(lm.y)
                 tracer_list.append(lm.z)
@@ -45,12 +57,35 @@ while True:
             ind=np.argmax(prob)
             confidence=prob[ind]
             label=le.inverse_transform([ind])[0] #it expects a list or array and 0 to make the output a string eg ["forward"] to "forward"
-            if confidence>=threshold:
+
+            if confidence>=normal_threshold:
+                control_mode="NORMAL"
                 predicted= label.upper()
+                if width>0.230:
+                    speed=1.0
+                    speed_lvl="FAST"
+                elif width>0.120:
+                    speed=0.7
+                    speed_lvl="MEDIUM"
+                else:
+                    speed=0.45
+                    speed_lvl="SLOW"
+            elif confidence>=crawl_theshold:
+                control_mode="LOW CONFIDENCE"
+                predicted= label.upper()
+                speed=0.20
+                speed_lvl="CRAWL"
             else:
+                control_mode="STOP"
                 predicted="STOP(NO ACTION)"
+                speed=0.0
+                speed_lvl="NONE"
+
     cv2.putText(image,f"Predicted:  {predicted}",(10,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
     cv2.putText(image,f"Confidence:  {confidence:.2f}",(10,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.putText(image,f"Control Mode:  {control_mode}",(10,120),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.putText(image,f"Palm width:  {width:.3f}",(10,160),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.putText(image,f"Speed Level:{speed_lvl} ({speed:.2f})",(10,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
 
 
     cv2.imshow("Live Hand Gesture Recognition", image)
